b8aebbae0ea55dba2c7ad01cdfd5bd401d7d8678
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-01-04 17:27:30 -0600

    front end gather form data

--- app/routes/_layout/index.tsx
+++ app/routes/_layout/index.tsx
@@ -11,11 +11,56 @@ export const Route = createFileRoute('/_layout/')({
 function Home() {
   const router = useRouter();
   const state = Route.useLoaderData();
-  const [playing, setPlaying] = useState(false);
+
+  const [formState, setFormState] = useState({
+    url: '',
+    ttl: 60, // seconds
+  });
+  const formIsValid = formState.url.length > 0 && formState.ttl > 0;
+
+  const onFormFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('form data:', formState);
+    // clear form
+    setFormState({ url: '', ttl: 60 });
+  };
 
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <p className="text-center">Link Shortener</p>
-    </div>
+    <form className="p-4 flex flex-col gap-2 mx-auto" onSubmit={onSubmit} style={{ maxWidth: 400 }}>
+      <p>Enter a URL to shorten:</p>
+      <label>
+        <div className="font-bold text-xs py-1">URL:</div>
+        <input
+          name="url"
+          type="url"
+          placeholder="https://www.spacex.com"
+          className="p-2 border border-gray-300 w-full"
+          value={formState.url}
+          onChange={onFormFieldChange}
+        />
+      </label>
+      <label>
+        <div className="font-bold text-xs py-1">TTL (seconds):</div>
+        <input
+          name="ttl"
+          type="number"
+          placeholder="Time to live (seconds)"
+          className="p-2 border border-gray-300 w-full"
+          value={formState.ttl}
+          onChange={onFormFieldChange}
+        />
+      </label>
+      <button
+        disabled={!formIsValid}
+        type="submit"
+        className="p-2 bg-peach-500 text-white hover:bg-peach-400"
+      >
+        Submit
+      </button>
+    </form>
   );
 }
