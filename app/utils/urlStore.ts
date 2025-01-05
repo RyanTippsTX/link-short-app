e64d11c5fd626686f1602bfa5467c661e912afd3
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-01-04 18:05:16 -0600

    url store

---
+++ app/utils/urlStore.ts
@@ -0,0 +1,37 @@
+import { createServerFn } from '@tanstack/start';
+
+export const getUrls = createServerFn({
+  method: 'GET',
+}).handler(async () => urlStore);
+
+export const getUrl = createServerFn({
+  method: 'GET',
+})
+  .validator((id: string) => id)
+  .handler(async ({ data: id }) => urlStore[id] || null);
+
+export const createUrl = createServerFn({
+  method: 'POST',
+})
+  .validator((data: { url: string; ttl: number }) => data)
+  .handler(async ({ data }) => {
+    const id = Math.random().toString(36).slice(2, 8);
+    urlStore[id] = { url: data.url, expiresAt: Date.now() + data.ttl * 1000 };
+    return id;
+  });
+
+interface URLStore {
+  [key: string]: { url: string; expiresAt: number };
+}
+
+const urlStore: URLStore = {
+  // dummy data
+  '01234567': {
+    url: 'https://www.spacex.com',
+    expiresAt: Date.now() + 1000 * 60 * 60 * 24,
+  },
+  '12345678': {
+    url: 'https://www.tesla.com',
+    expiresAt: Date.now() + 1000 * 60 * 60 * 24,
+  },
+};
