b05a528c7222147cc74b9ef7b18846ce4d0f1f7c
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-02-19 02:27:06 -0600

    use redis for url store

--- app/routes/_layout/list.tsx
+++ app/routes/_layout/list.tsx
@@ -20,43 +20,44 @@ function PageComponent() {
 
   return (
     <div>
-      {Object.keys(state.activeUrlStore).map((id) => {
-        const { url: url, expiresAt } = state.activeUrlStore[id];
-        const shortUrl = `${host}/${id}`;
-        return (
-          <div key={id} className="p-2 border-b border-gray-300 whitespace-nowrap">
-            {/* expiration */}
-            <div className="text-sm text-gray-500">
-              Expires at {new Date(expiresAt).toLocaleString()}
-            </div>
-            <div className="flex items-center flex-wrap">
-              {/* short url */}
-              <div className="text-gray-500">
-                <a
-                  href={shortUrl}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="font-bold text-peach-500  hover:underline"
-                >
-                  {shortUrl}
-                </a>
+      {state
+        .filter(({ url }) => !!url)
+        .map(({ id, url, expiresAt }) => {
+          const shortUrl = `${host}/${id}`;
+          return (
+            <div key={id} className="p-2 border-b border-gray-300 whitespace-nowrap">
+              {/* expiration */}
+              <div className="text-sm text-gray-500">
+                Expires at {new Date(expiresAt).toLocaleString()}
               </div>
-              {/* url */}
-              <div className="flex-1">
-                <span className="mx-2">{'→'}</span>
-                <a
-                  href={url}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="font-bold text-peach-500 hover:underline"
-                >
-                  {url}
-                </a>
+              <div className="flex items-center flex-wrap">
+                {/* short url */}
+                <div className="text-gray-500">
+                  <a
+                    href={shortUrl}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-bold text-peach-500  hover:underline"
+                  >
+                    {shortUrl}
+                  </a>
+                </div>
+                {/* url */}
+                <div className="flex-1">
+                  <span className="mx-2">{'→'}</span>
+                  <a
+                    href={url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-bold text-peach-500 hover:underline"
+                  >
+                    {url}
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
--- app/utils/urlStore.ts
+++ app/utils/urlStore.ts
@@ -1,55 +1,43 @@
 import { createServerFn } from '@tanstack/start';
+import { redis } from './redis';
 // import { getRequestHeaders } from 'vinxi/http';
 
 export const getUrls = createServerFn({ method: 'GET' }).handler(async () => {
-  // const host = getRequestHeaders().host;
-  const activeUrlStore = Object.entries(urlStore).reduce<URLStore>((acc, [id, data]) => {
-    if (data.expiresAt > Date.now()) {
-      acc[id] = data;
-    }
-    return acc;
-  }, {});
+  const keys = await redis.keys('url:*');
+  const urlDataList = await Promise.all(
+    keys.map(async (key) => {
+      const urlData = (await redis.hgetall(key)) as any as UrlData;
+      return {
+        id: key.split(':')[1],
+        ...urlData,
+      };
+    }),
+  );
 
-  return {
-    activeUrlStore,
-    //  host
-  };
+  return urlDataList;
 });
 
 export const getUrl = createServerFn({ method: 'GET' })
   .validator((id: string) => id)
   .handler(async ({ data: id }) => {
-    const urlData = urlStore[id];
-    if (urlData && urlData.expiresAt > Date.now()) {
-      return urlData;
-    }
-    return null;
+    const urlData = await redis.hgetall(`url:${id}`);
+    return urlData as UrlData | null;
   });
 
 export const createUrl = createServerFn({ method: 'POST' })
   .validator((data: { url: string; ttl: number }) => data)
   .handler(async ({ data }) => {
     const id = Math.random().toString(36).slice(2, 10);
-    urlStore[id] = { url: data.url, expiresAt: Date.now() + data.ttl * 1000 };
+    const urlData = {
+      url: data.url,
+      expiresAt: Date.now() + data.ttl * 1000,
+    } satisfies UrlData;
+    await redis.hmset(`url:${id}`, urlData);
+    await redis.pexpireat(`url:${id}`, urlData.expiresAt);
     return id;
   });
 
-interface URLStore {
-  [key: string]: { url: string; expiresAt: number };
-}
-
-const urlStore: URLStore = {
-  // dummy data
-  '01234567': {
-    url: 'https://www.spacex.com',
-    expiresAt: Date.now() + 1000 * 60 * 60 * 24,
-  },
-  '12345678': {
-    url: 'https://www.tesla.com',
-    expiresAt: Date.now() + 1000 * 60 * 60 * 24,
-  },
-  '23456789': {
-    url: 'https://www.paypal.com',
-    expiresAt: Date.now() - 1000 * 60 * 60 * 24,
-  },
+type UrlData = {
+  url: string;
+  expiresAt: number;
 };
